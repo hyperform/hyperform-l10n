8ea7d1080b2d747d1b246eeaba2225dbc8c604d0
Marco Aurélio Dilda Campos <madcampos@outlook.com>
2016-10-18 20:54:11 -0300

    Add pt-BR translation
    Added brazilian portuguese translation (based on the `de` and `pt-PT` files) and fixed a typo for pt-PT.

---
+++ dist/hyperform.pt-BR.js
@@ -0,0 +1,39 @@
+/**
+ * source: Mozilla
+ * http://mxr.mozilla.org/l10n-central/source/pt-BR/dom/chrome/dom/dom.properties?raw: 1
+ * published under the MPL v2.0
+ */
+hyperform.add_translation("pt-BR",{
+TextTooLong: "Por favor, diminua o texto para %l caracteres ou menos (no momento ele tem %l caracteres).",
+//TextTooShort: "Please use at least %l characters (you are currently using %l characters).",
+ValueMissing: "Por favor, preencha este campo.",
+CheckboxMissing: "Por favor, marque esta opção se deseja continuar.",
+RadioMissing: "Por favor, selecione uma destas opções.",
+FileMissing: "Por favor, selecione um arquivo.",
+SelectMissing: "Por favor, selecione um item da lista.",
+InvalidEmail: "Por favor, digite um endereço de e-mail.",
+InvalidURL: "Por favor, forneça um endereço da internet.",
+PatternMismatch: "Por favor, satisfaça o formato requisitado.",
+PatternMismatchWithTitle: "Por favor, satisfaça o formato requisitado: %l.",
+NumberRangeOverflow: "Por favor, selecione um valor que não seja maior que %l.",
+DateRangeOverflow: "Por favor, selecione um valor que não seja depois de %l.",
+TimeRangeOverflow: "Por favor, selecione um valor que não seja depois de %l.",
+NumberRangeUnderflow: "Por favor, selecione um valor que não seja menor que %l.",
+DateRangeUnderflow: "Por favor, selecione um valor que não seja antes de %l.",
+TimeRangeUnderflow: "Por favor, selecione um valor que não seja antes de %l.",
+StepMismatch: "Por favor, selecione um valor válido. Os dois valores válidos mais próximos são %l e %l.",
+StepMismatchOneValue: "Por favor, selecione um valor válido. O valor válido mais próximo é %l.",
+BadInputNumber: "Por favor, digite um número.",
+});
+hyperform.add_translation("pt-BR",
+{
+  "Please match the requested type.": "Por favor, satisfaça o tipo requisitado.",
+  "Please comply with all requirements.": "Por favor, satisfaça todos os requisitos.",
+  "Please lengthen this text to %l characters or more (you are currently using %l characters).": "Por favor, use pelo menos %l caracteres (no momento ele tem %l caracteres).",
+  "Please use the appropriate format.": "Por favor, use o formato apropriado.",
+  "Please enter a comma separated list of email addresses.": "Por favor, digite uma lista de e-mails separados por vírgula.",
+  "Please select a file of the correct type.": "Por favor, selecione um arquivo do tipo correto.",
+  "Please select one or more files.": "Por favor, selecione um ou mais arquivos."
+}
+);
+hyperform.set_language("pt-BR");
