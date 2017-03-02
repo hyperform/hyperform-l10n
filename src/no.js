700ca1745c7649f14d98de21b68dec5d1d2c53bd
George Gooding <george@nettsentrisk.no>
2017-03-02 12:28:40 +0100

    Adding Norwegian translation

---
+++ src/no.js
@@ -0,0 +1,28 @@
+/**
+ * source: Mozilla
+ * https://hg.mozilla.org/l10n-central/da/raw-file/default/dom/chrome/dom/dom.properties
+ * published under the MPL v2.0
+ */
+hyperform.addTranslation("no",{
+TextTooLong:"Begrens teksten til %l tegn (%l tegn er brukt).",
+TextTooShort:"Bruk minst %l tegn (%l tegn er brukt).",
+ValueMissing:"Fyll ut dette feltet.",
+CheckboxMissing:"Huk av dette feltet for å fortsette.",
+RadioMissing:"Velg minst ett av alternativene.",
+FileMissing:"Velg en fil.",
+SelectMissing:"Velg ett av alternativene fra listen.",
+InvalidEmail:"Tast inn en gyldig epostadresse.",
+InvalidURL:"Tast inn en gyldig nettadresse.",
+PatternMismatch:"Formatet på inntastet data er ugyldig.",
+PatternMismatchWithTitle:"Overhold formatet: %l.",
+NumberRangeOverflow:"Velg en verdi mindre enn %l.",
+DateRangeOverflow:"Velg en dato før %l.",
+TimeRangeOverflow:"Velg et klokkeslett før %l.",
+NumberRangeUnderflow:"Velg en verdi mer enn %l.",
+DateRangeUnderflow:"Velg en dato etter %l.",
+TimeRangeUnderflow:"Velg et klokkeslett etter %l.",
+StepMismatch:"Velg en gyldig verdi. De nærmeste gyldige verdiene er %l og %l",
+StepMismatchOneValue:"Velg en gyldig verdi. Nærmeste gyldig verdi er %l.",
+BadInputNumber:"Tast inn et tall.",
+});
+hyperform.setLanguage("no");
