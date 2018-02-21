4f80067acd0a0a995f59fbae92e9d67c2e03b76c
Roy Lodder <roy@roylodder.com>
2018-02-21 14:14:05 +0100

    add Dutch translation (nl)

---
+++ dist/hyperform.nl.cjs.js
@@ -0,0 +1,38 @@
+var hyperform = require("hyperform");
+/**
+ * some of the following strings were imported from Mozilla's
+ * Firefox source repository, found at
+ * https://hg.mozilla.org/l10n-central/nl/raw-file/default/dom/chrome/dom/dom.properties
+ * published under the MPL v2.0
+ */
+hyperform.addTranslation('nl', {
+TextTooLong: "Kort deze tekst in tot %l tekens of minder (u gebruikt nu %l tekens).",
+TextTooShort: "Gebruik minstens %l tekens (u gebruikt momenteel %l tekens).",
+ValueMissing: "Vul dit veld in.",
+CheckboxMissing: "Vink dit vakje aan als u wilt doorgaan.",
+RadioMissing: "Selecteer een van deze opties.",
+FileMissing: "Selecteer een bestand.",
+SelectMissing: "Selecteer een item in de lijst.",
+InvalidEmail: "Voer een e-mailadres in.",
+InvalidURL: "Voer een URL in.",
+InvalidDate: "Voer een geldige datum in.",
+PatternMismatch: "Gebruik de gevraagde indeling.",
+PatternMismatchWithTitle: "Gebruik de gevraagde indeling: %l.",
+NumberRangeOverflow: "Selecteer een waarde die niet hoger is dan %l.",
+DateTimeRangeOverflow: "Selecteer een waarde die niet later is dan %l.",
+NumberRangeUnderflow: "Selecteer een waarde die niet lager is dan %l.",
+DateTimeRangeUnderflow: "Selecteer een waarde die niet vroeger is dan %l.",
+StepMismatch: "Selecteer een geldige waarde. De twee dichtstbijzijnde geldige waarden zijn %l en %l.",
+StepMismatchOneValue: "Selecteer een geldige waarde. De dichtstbijzijnde geldige waarde is %l.",
+BadInputNumber: "Voer een getal in.",
+"Please match the requested type.": "Pas de invoer aan aan het vereiste type.",
+"Please comply with all requirements.": "Voldoe aan alle vereisten.",
+"Please lengthen this text to %l characters or more (you are currently using %l characters).": "Verleng deze tekst tot ten minste %l tekens (u gebruikt momenteel %l tekens).",
+"Please use the appropriate format.": "Gebruik de juiste indeling.",
+"Please enter a comma separated list of email addresses.": "Voer een door komma's gescheiden lijst van e-mailadressen in.",
+"Please select a file of the correct type.": "Selecteer een bestand van het juiste type.",
+"Please select one or more files.": "Selecteer een of meer bestanden.",
+"any value":"elke waarde",
+"any valid value":"elke geldige waarde",
+});
+hyperform.setLanguage('nl');
